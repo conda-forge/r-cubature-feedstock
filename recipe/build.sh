f168736984aa6114b9e00b91f48e9da7c6039480
Mervin Fansler <mef3005@med.cornell.edu>
2023-06-19 12:48:59 -0400

    cc17

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,8 @@ export DISABLE_AUTOBREW=1
 # get updated config.guess and config.sub
 cp $BUILD_PREFIX/share/gnuconfig/config.{sub,guess} src/Cuba-4.2/.
 
+# R 4.3 expects 'CC17' env var
+export CC17=${CC}
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
